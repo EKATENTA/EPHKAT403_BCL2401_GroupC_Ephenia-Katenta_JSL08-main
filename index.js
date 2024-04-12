8a12f29aba3ae1b69c0b91e85d9cc99ca0610f7b
Ephenia Katenta <katentaephenia@gmail.com>
2024-04-12 02:38:55 +0200

    Implement singleton pattern for BankBranch class

---
+++ index.js
@@ -0,0 +1,15 @@
+//// Define the BankBranch class
+class BankBranch {
+  constructor(branchInfo) {
+    if (!BankBranch.bankBranchInstance) {
+      this.branchInfo = branchInfo;
+      BankBranch.bankBranchInstance = this;
+    }
+    return BankBranch.bankBranchInstance;
+  }
+
+  // Method to retrieve branch information
+  getBranchInfo() {
+    return this.branchInfo;
+  }
+}
